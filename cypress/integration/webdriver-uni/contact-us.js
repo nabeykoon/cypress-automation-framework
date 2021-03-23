b36585f586fbe57836ebdf1f2c318f5543ad7861
Nadeera Abeykoon <nabeykoon@gmail.com>
2021-03-24 00:49:31 +0530

    add more assertions

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -7,6 +7,9 @@ describe("Test contact Us form via WebDriverUniversity", () => {
         //cy.get("#contact-us").click();
         // older cypress version Ex: 4.5.0, you should use click with argument click({force: true}) since this element dimension is 0X0 pixels and it is not visible.
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+        cy.title().should('include', 'WebDriver | Contact Us');
+        cy.url().should('include', 'contactus');
         cy.get('input[name="first_name"]').type("Nadeera");
         cy.get('input[name="last_name"]').type("Abeykoon");
         cy.get('input[name="email"]').type("test1984@gmail.com");
@@ -15,7 +18,7 @@ describe("Test contact Us form via WebDriverUniversity", () => {
         cy.xpath('//div[@id="contact_reply"]//h1').should('have.text', "Thank You for your Message!");
     })
 //run only one test 'it.only' 
-    it.only("Should NOT be able to submit a submission via Contact Us form due to all mandatory fields", ()=>{
+    it("Should NOT be able to submit a submission via Contact Us form due to all mandatory fields", ()=>{
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
         cy.get('input[name="first_name"]').type("Supipi");
         cy.get('input[name="last_name"]').type("Abeykoon");
