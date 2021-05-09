52e0d40ba81e9d987e1568983f4ec300691a35c6
Nadeera Abeykoon <nabeykoon@gmail.com>
2021-05-10 01:25:48 +0530

    Add examples for checkbox and radio button elements

---
+++ cypress/integration/webdriver-uni/checkboxes.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+
+describe("Verify Checkboxes via Webdriver uni", () => {
+    it("check and validate checkbox", ()=>{
+        
+        cy.visit("http://www.webdriveruniversity.com/");
+        // invoke jQuery method 'removeAttr' to remove attribute "target="_blank" which triggered the new tab opening after click on the link
+        cy.get("#dropdown-checkboxes-radiobuttons").invoke("removeAttr", "target").click();
+        
+        cy.get("input[value='option-1']").check().should("be.checked");
+    })
+
+    it("uncheck and verify it is unchecked", ()=>{
+        
+        cy.visit("http://www.webdriveruniversity.com/");
+        // invoke jQuery method 'removeAttr' to remove attribute "target="_blank" which triggered the new tab opening after click on the link
+        cy.get("#dropdown-checkboxes-radiobuttons").invoke("removeAttr", "target").click();
+        
+        cy.get("input[value='option-3']").uncheck().should("not.be.checked");
+    })
+
+    it("Check mutiple checkboxes", ()=>{
+        
+        cy.visit("http://www.webdriveruniversity.com/");
+        // invoke jQuery method 'removeAttr' to remove attribute "target="_blank" which triggered the new tab opening after click on the link
+        cy.get("#dropdown-checkboxes-radiobuttons").invoke("removeAttr", "target").click();
+        
+        cy.get("input[type='checkbox']").check(["option-1", "option-2", "option-3", "option-4"]).should("be.checked");
+    })
+})
