72922578c14e2b300729d514a54b02ba3062d173
Nadeera Abeykoon <nabeykoon@gmail.com>
2021-05-23 18:23:04 +0530

    Update mouse acitions script

--- cypress/integration/webdriver-uni/mouse-actions.js
+++ cypress/integration/webdriver-uni/mouse-actions.js
@@ -23,17 +23,17 @@ describe("Verify different mouse actions  via webdriver uni", () => {
         
         cy.visit("http://www.webdriveruniversity.com/");
         // invoke jQuery method 'removeAttr' to remove attribute "target="_blank" which triggered the new tab opening after click on the link
-        cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click();
+        cy.get("#actions").invoke("removeAttr", "target").click();
 
         cy.get("#double-click").dblclick();
         
     });
 
-    it.only("I should be able to hold down the left mouse click on a given element", ()=>{
+    it("I should be able to hold down the left mouse click on a given element", ()=>{
         
         cy.visit("http://www.webdriveruniversity.com/");
         // invoke jQuery method 'removeAttr' to remove attribute "target="_blank" which triggered the new tab opening after click on the link
-        cy.get("#actions").scrollIntoView().invoke("removeAttr", "target").click();
+        cy.get("#actions").invoke("removeAttr", "target").click();
 
         cy.get("#click-box").trigger('mousedown').then(($element) => {
             expect($element).to.have.css('background-color', 'rgb(0, 255, 0)');
@@ -41,5 +41,14 @@ describe("Verify different mouse actions  via webdriver uni", () => {
         })
         
     });
-    
+
+    it.only("I should be able to mouse hover and click on a popup list item", ()=>{
+        
+        cy.visit("http://www.webdriveruniversity.com/");
+        // invoke jQuery method 'removeAttr' to remove attribute "target="_blank" which triggered the new tab opening after click on the link
+        cy.get("#actions").invoke("removeAttr", "target").click();
+
+        cy.get(".dropbtn").contains("Hover Over Me First!").trigger('mouseover');
+        cy.get(".list-alert").contains("Link 1").click({force: true});
+    });
 })
