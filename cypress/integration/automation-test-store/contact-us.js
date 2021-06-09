0b83b57884f65757255e2329e5465346d225cd28
Nadeera Abeykoon <nabeykoon@gmail.com>
2021-06-10 02:07:37 +0530

    Update contact-us.js using cy fixtures to be data driven

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -1,14 +1,21 @@
 /// <reference types="Cypress" />
 
 describe("Test contact Us form via Automation Test Store", () => {
+
+    before(() => {
+        cy.fixture("userDetails").as("user");
+    })
     it("Should be able to submit a successful submission via Contact Us form", ()=>{
         
         cy.visit('https://automationteststore.com/');
         cy.get('a[href$="contact"]').click().then((linkText) => {
             cy.log("Selected link text: " + linkText.text())
         }); //xpath -> //a[text()='Contact Us']
-        cy.get('#ContactUsFrm_first_name').type('Nadeera');
-        cy.get('#ContactUsFrm_email').type('test@gmail.com');
+        cy.get("@user").then((user) => {
+            cy.get('#ContactUsFrm_first_name').type(user.first_name);
+            cy.get('#ContactUsFrm_email').type(user.email);
+        })
+      
         cy.get('#ContactUsFrm_email').should('have.attr', 'name', 'email');
         cy.get('#ContactUsFrm_enquiry').type('Do you have Nvidia GTX 3060 in stock');
         //xpath locators can be used with the library cypress-io/cypress-xpath
--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,6 +1,12 @@
 /// <reference types="Cypress" />
 
 describe("Test contact Us form via WebDriverUniversity", () => {
+    before(() =>{
+        cy.fixture('example.json').then((data) => {
+            //this.data = data;
+            globalThis.data = data;
+        })
+    })
     it("Should be able to submit a successful submission via Contact Us form", ()=>{
         
         cy.visit("http://www.webdriveruniversity.com/");
@@ -11,9 +17,9 @@ describe("Test contact Us form via WebDriverUniversity", () => {
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
         cy.title().should('include', 'WebDriver | Contact Us');
         cy.url().should('include', 'contactus');
-        cy.get('input[name="first_name"]').type("Nadeera");
-        cy.get('input[name="last_name"]').type("Abeykoon");
-        cy.get('input[name="email"]').type("test1984@gmail.com");
+        cy.get('input[name="first_name"]').type(data.first_name);
+        cy.get('input[name="last_name"]').type(data.last_name);
+        cy.get('input[name="email"]').type(data.email);
         cy.get('textarea[name="message"]').type("This is a submission of test contact us page");
         cy.get('input[type="submit"]').click();
         cy.xpath('//div[@id="contact_reply"]//h1').should('have.text', "Thank You for your Message!");
@@ -21,8 +27,8 @@ describe("Test contact Us form via WebDriverUniversity", () => {
 //run only one test 'it.only' 
     it("Should NOT be able to submit a submission via Contact Us form due to all mandatory fields", ()=>{
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
-        cy.get('input[name="first_name"]').type("Supipi");
-        cy.get('input[name="last_name"]').type("Abeykoon");
+        cy.get('input[name="first_name"]').type(data.first_name);
+        cy.get('input[name="last_name"]').type(data.last_name);
         cy.get('textarea[name="message"]').type("This is a submission of test contact us page");
         cy.get('input[type="submit"]').click();
         cy.get('body').contains('Error: all fields are required')
