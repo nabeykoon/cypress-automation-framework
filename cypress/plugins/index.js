1a2d5db34ec253fe8f617e6ac631c7b659e1871f
Nadeera Abeykoon <nadeera.abeykoon@cambio.lk>
2021-09-28 15:06:46 +0530

    Add lighthouse configuration and sample test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -41,3 +41,17 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 }
+
+//Configure google lighthouse and p11ly
+const { lighthouse, pa11y, prepareAudit } = require("cypress-audit");
+
+module.exports = (on, config) => {
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+  });
+
+  on("task", {
+    lighthouse: lighthouse(), // calling the function is important
+    pa11y: pa11y(), // calling the function is important
+  });
+};
