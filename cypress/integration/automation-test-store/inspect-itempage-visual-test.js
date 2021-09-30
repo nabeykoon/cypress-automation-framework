c06f9e73b58e4a213e685efbc9adc383de9539d5
Nadeera Abeykoon <nadeera.abeykoon@cambio.lk>
2021-09-30 17:36:42 +0530

    Update visual test related code

--- cypress/integration/automation-test-store/inspect-itempage-visual-test.js
+++ cypress/integration/automation-test-store/inspect-itempage-visual-test.js
@@ -11,10 +11,16 @@ describe("Inspect automation test store items using chain of commands", () => {
       .then(function (itemHeaderText) {
         cy.log("Selected the following item " + itemHeaderText.text());
       });
+      
     cy.eyesOpen({
       appName: "Automation Store",
       testName: "inspect item",
+      browser: [
+        {name: 'chrome', width: 1024, height: 768},
+        {name: 'firefox', width: 1024, height: 768},
+      ]
     });
+    cy.wait(3000);
     cy.eyesCheckWindow({tag: 'item widget page'});
     cy.eyesClose();
   });
